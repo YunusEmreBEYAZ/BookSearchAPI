0067fe46ec55e4f1e44c8c91513398d3ea96aa7e
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-02-26 23:45:14 +0100

    little improvements

--- src/page.js
+++ src/page.js
@@ -1,42 +1,42 @@
 import { initView } from './view.js';
 
 export function initPage() {
-    document.body.innerHTML = ''; // for cleaning the body before using
     initView();  //called the elements 
-    document.getElementById('button').addEventListener('click', search);
-    document.getElementById('input').addEventListener('keyup', (e) => {
-        if(e.key === 'Enter'){
-            search();
-        }
-    })
-
-    async function search(data) {      
-        document.getElementById('info-div').innerHTML = '';  
-        try {
-            const response = await fetchData(data);
-
-        } catch (error) {
-            alert(error.message)
-        }
-        new Error('Request failed!');
-    }
-    
+        document.getElementById('button').addEventListener('click', search);
+        document.getElementById('input').addEventListener('keyup', (e) => {
+            if(e.key === 'Enter'){
+                search();
+            }
+        }) 
 
 }
 
-function results(data){
+async function search(data) {      
+    document.getElementById('info-div').innerHTML = '';  
+    try {
+        const response = await fetchData(data);
+
+    } catch (error) {
+        alert(error.message)
+    }
+    new Error('Request failed!');
+}
+
+function results(bookInfo){
+
     const img = document.createElement('img');
     document.getElementById('info-div').appendChild(img);
-    img.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+    img.src = bookInfo.items[0].volumeInfo.imageLinks.thumbnail;
     const bookName = document.createElement('h2');
     document.getElementById('info-div').appendChild(bookName);
-    bookName.textContent = data.items[0].volumeInfo.title;
+    bookName.textContent = bookInfo.items[0].volumeInfo.title;
     const author = document.createElement('h2');
     document.getElementById('info-div').appendChild(author);
-    author.textContent = data.items[0].volumeInfo.authors[0];
+    author.textContent = bookInfo.items[0].volumeInfo.authors[0];
     const description = document.createElement('p');
     document.getElementById('info-div').appendChild(description);
-    description.textContent = data.items[0].volumeInfo.description;
+    description.textContent = bookInfo.items[0].volumeInfo.description;
+
 }
 
 function fetchData(data) {
@@ -45,7 +45,7 @@ let inputValue = document.getElementById('input').value;
 return fetch(`https://www.googleapis.com/books/v1/volumes?q=${input.value}&key=AIzaSyDs1Ksy7B96SOovc8LZATe1d8p-RcvCLL0`)
     .then((response) => {
         if(!response.ok){
-            document.getElementById('info-div').innerHTML = 'Fetch not responding'
+            document.getElementById('info-div').innerHTML = 'Fetch not responding';
         }
         return response.json()
     })
