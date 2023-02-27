ad1bb88c73835fdbc77742292f080dc285b41897
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-02-27 22:00:32 +0100

    working on other books

--- src/page.js
+++ src/page.js
@@ -18,14 +18,26 @@ export function initPage() {
 async function search(data) {      
       
     try {
-        const response = await fetchData(data);
-        results(response);
+        const bookURL = `https://www.googleapis.com/books/v1/volumes?q=${input.value}&key=AIzaSyDs1Ksy7B96SOovc8LZATe1d8p-RcvCLL0`;
+        const fetchedBook = await fetchData(bookURL);
+        const searchedBookTitle = fetchedBook.items[0].volumeInfo.title;
+        console.log(searchedBookTitle);
+        const searchedAuthor = fetchedBook.items[0].volumeInfo.authors[0];
+        const authorURL = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${searchedAuthor}&key=AIzaSyDs1Ksy7B96SOovc8LZATe1d8p-RcvCLL0`;
+        const fetchedAuthor = await fetchData(authorURL);
+        const authorBooks = fetchedAuthor.items;
+        const otherBooks = authorBooks.filter(book => book.volumeInfo.title !== searchedBookTitle);
+        const otherBookTitles = otherBooks.map(book => book.volumeInfo.title);
+        results(fetchedBook, otherBookTitles); 
     } catch (error) {
         document.getElementById('info-section').innerHTML = `${error.statusText}`;
     }
+
 }
 
-function results(bookInfo){
+
+
+function results(bookInfo, otherBookTitles){
     document.getElementById('img-section').innerHTML = '';
     document.getElementById('book-author-section').innerHTML = '';
     document.getElementById('description-section').innerHTML = '';
@@ -41,11 +53,11 @@ function results(bookInfo){
     const description = document.createElement('p');
     document.getElementById('description-section').appendChild(description);
     description.textContent = `Description: ${bookInfo.items[0].volumeInfo.description}`;
-
+    console.log(otherBookTitles)
 }
 
-function fetchData(data) {
-return fetch(`https://www.googleapis.com/books/v1/volumes?q=${input.value}&key=AIzaSyDs1Ksy7B96SOovc8LZATe1d8p-RcvCLL0`)
+function fetchData(url) {
+return fetch(url)
     .then((response) => {
         if(!response.ok){
             document.getElementById('info-section').innerHTML = 'Fetch not responding';
